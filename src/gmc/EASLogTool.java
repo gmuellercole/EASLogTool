b6d2153b32e790be311c954f67e56d0059b5c60a
Greg Cole <greg.cole@comcast.net>
2014-06-11 22:31:36 -0700

    rename groker

---
+++ src/gmc/EASLogTool.java
@@ -0,0 +1,10 @@
+package gmc;
+
+import java.io.IOException;
+import java.util.List;
+
+// TODOs:
+// details for FolderSync
+
+
+// This would be a more useful tool with the following enhancements:
